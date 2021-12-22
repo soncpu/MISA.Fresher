b2659004eca1a0172cb3049083c7b1d75eb142fe
Vũ Văn Sơn <63628047+soncpu@users.noreply.github.com>
2021-12-22 12:00:55 +0700

    Son

---
+++ MISA.Fresher.CukCuk.VuVanSon/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.Fresher.CukCuk.VuVanSon.Model;
+
+namespace MISA.Fresher.CukCuk.VuVanSon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName(string? name, string? code)
+        {
+            return Ok(name);
+        }
+        [HttpPost]
+        public IActionResult PostCustomer([FromBody] Customer customer)
+        {
+            return Ok(customer);
+        }
+        [HttpPut]
+        public IActionResult PUT(Customer customer)
+        {
+            return Ok(customer);
+        }
+        [HttpDelete("id")]
+        public IActionResult delete (Customer customer)
+        {
+            return Ok(customer);
+        }
+    }
+}
---
+++ MISA.Fresher.CukCuk.VuVanSon/Model/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace MISA.Fresher.CukCuk.VuVanSon.Model
+{
+    public class Customer
+    {
+        public string CustomerCode { set; get;}
+        public string FullName   {set; get;}
+    }
+}
